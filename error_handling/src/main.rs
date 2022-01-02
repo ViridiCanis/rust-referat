4561ce1103e6e05272b524cea1deabafb626a00a
Viridi <virididev@gmail.com>
2022-01-02 15:24:12 +0100

    error handling

---
+++ error_handling/src/main.rs
@@ -0,0 +1,36 @@
+use std::fs;
+use std::io::stdin;
+
+fn main() {
+    // zwei Arten von Fehlern:
+    //   behebbare
+    //   nicht behebbare
+
+    // nicht behebbar
+
+    // panic!("fataler Fehler");
+
+    // fs::read("nicht_existierende_datei").unwrap();
+
+
+    // behebbar
+
+    let mut num: i32 = 0;
+    loop {
+        // neuer leerer String, da read_line am Ende anhängt
+        let mut input = String::new();
+        print!("Zahl: ");
+        stdin().read_line(&mut input).expect("failed reading from stdin");
+
+        // trim, weil \n im String ist
+        match input.trim().parse() {
+            Ok(val) => {
+                num = val;
+                break;
+            },
+            Err(err) => println!("Bitte gebe eine gültige Zahl ein! ({}): {}", err, input)
+        }
+    }
+
+    println!("gewählte Zahl: {}", num);
+}
